a7249704550186654a9e215216194d5edd3666e6
jhk0530 <hwanistic@gmail.com>
2019-12-14 19:02:00 +0900

    Update shinyRoughJSbasic.js

--- inst/htmlwidgets/shinyRoughJSbasic.js
+++ inst/htmlwidgets/shinyRoughJSbasic.js
@@ -3,25 +3,27 @@ HTMLWidgets.widget({
   type : 'output',
 
   factory : function(el, width, height) {
+	var container = document.getElementById(el.id);
+	
+	
+	// Initialisation
+	var cv = document.createElement("canvas");
+	cv.id = 'minicv'; // mini canvas
+	container.appendChild(cv)
+	cv.setAttribute('width',container.clientWidth)
+	cv.setAttribute('height',container.clientHeight)
 
-    // Initialisation
-    var cv = document.createElement("canvas");
-    cv.setAttribute('width', $("#cv").width());
-    cv.setAttribute('height',$("#cv").height());
-    cv.id = 'minicv'; // mini canvas
-    document.getElementById('cv').appendChild(cv); // add mini canvas to div cv
-    
     var rc = rough.canvas(document.getElementById('minicv'));
     return {
       renderValue: function(input) {
         var Items = input.items;
         for( var i = 0; i<Items.length; i++){
-          var thisItem = Items[i];                    
-          if(thisItem.type=='rectangle'){                      
+          var thisItem = Items[i];
+          if(thisItem.type=='rectangle'){
             rc.rectangle(thisItem.x, thisItem.y, thisItem.w, thisItem.h, thisItem.options)
-          }                 
-        }        
-      }      
+          }
+        }
+      }
     }
   }
 });
